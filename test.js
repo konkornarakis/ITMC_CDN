eee080f7b96ee4ca62b0134d7bfc80041eca039a
sahgon <konkornarakis@gmail.com>
2023-02-06 17:39:11 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+$("test1").on("click", function () {
+    console.log("test1 clicked!");
+});
+
+document.getElementById("test2").addEventListener("click", () => {
+    console.log("test2 clicked!");
+})
